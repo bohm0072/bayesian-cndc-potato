8e0978b4030e7be324df98b4dcc8cc881a271cfc
Brian Bohman <23647616+bohm0072@users.noreply.github.com>
2020-12-13 11:48:12 -0600

    Adding model results for model with all varieties

---
+++ brms_eval.R
@@ -0,0 +1,9 @@
+
+m0 <- readRDS("Models/model-1_Bohman_Minnesota_Clearwater-DakotaRusset-Easton-RussetBurbank-Umatilla.rds")
+m1 <- readRDS("Models/model-1_Bohman_Minnesota_RussetBurbank.rds")
+m2 <- readRDS("Models/model-1_Bohman_Minnesota_Clearwater.rds")
+m3 <- readRDS("Models/model-1_Bohman_Minnesota_Umatilla.rds")
+m4 <- readRDS("Models/model-1_Bohman_Minnesota_DakotaRusset.rds")
+m5 <- readRDS("Models/model-1_Bohman_Minnesota_Easton.rds")
+
+m1$fit
