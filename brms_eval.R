08714c228b86ce0c1b47f61a06f327aad59be76b
Brian Bohman <23647616+bohm0072@users.noreply.github.com>
2020-12-13 11:24:26 -0600

    Update script and function to fit brms models

---
+++ brms_eval.R
@@ -0,0 +1,5 @@
+m1 <- readRDS("Models/model-1_Bohman_Minnesota_RussetBurbank.rds")
+m2 <- readRDS("Models/model-1_Bohman_Minnesota_Clearwater.rds")
+m3 <- readRDS("Models/model-1_Bohman_Minnesota_Umatilla.rds")
+m4 <- readRDS("Models/model-1_Bohman_Minnesota_DakotaRusset.rds")
+m5 <- readRDS("Models/model-1_Bohman_Minnesota_Easton.rds")
